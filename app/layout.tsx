fffcc8105a624a51d4d19bdc201c6d10078a939c
Arjun Karmaker <arjunkarmaker03@gmail.com>
2024-06-03 00:22:38 +0600

    clerk initialization complete
    just installed clerk and added as middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], variable: '--font-poppins' });
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,26 @@
+import { SignedOut } from "@clerk/nextjs"
+import Link from "next/link"
+import { Button } from "../ui/button"
 
 const Header = () => {
   return (
-    <div>header</div>
+    <header className="w-full">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <h1 className="text-2xl font-bold text-slate-900">CEMS</h1>
+        </Link>
+
+        <div className="flex w-32 justify-end">
+          <SignedOut>
+            <Button asChild size='lg' className="rounded-xl">
+              <Link href="/login">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
   )
 }
 
